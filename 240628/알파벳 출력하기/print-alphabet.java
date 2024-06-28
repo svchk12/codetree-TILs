a57757d513537d8792ab262ffba90a8198019b49
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-06-28 10:57:43 +0900

    Title: 알파벳 출력하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240628/알파벳 출력하기/print-alphabet.java
+++ 240628/알파벳 출력하기/print-alphabet.java
@@ -3,12 +3,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int x = 'A';
+        int x = 'A'; //65
         
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 System.out.print((char)x);
                 x++;
+                if(x > 91){
+                    x = 65;
+                }
             }
             System.out.println();
         }
