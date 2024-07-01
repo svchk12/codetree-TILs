e41158e7aca18d15300c25f2d36cda0d44102eb3
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-01 15:18:44 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240701/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        
+        
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            double avg = 0.0;
+            int[] arr = new int[4];
+            for(int j = 0; j < 4; j++){
+                arr[j] = sc.nextInt();
+                sum = sum + arr[j];
+                avg = (double)sum / 4;
+                
+            }
+            if(avg >= 60){
+                System.out.println("pass");
+                cnt++;
+            }else{
+                System.out.println("fail");
+            }
+        }
+        System.out.println(cnt);
+    }
+}
