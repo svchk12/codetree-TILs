93cd27aa378089d8367548fc624a5f4309c10828
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-01 17:40:56 +0900

    Title: 전항의 두 배, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240701/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int [] arr = new int[10];
+
+        arr[0] = a;
+        arr[1] = b;
+        System.out.print(arr[0] + " ");
+        System.out.print(arr[1] + " ");
+
+        for(int i = 2; i < 10; i++){
+            arr[i] = arr[i-1] + 2*arr[i-2];
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
