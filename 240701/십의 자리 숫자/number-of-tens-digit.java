f582c45e234bcc93c91eb7c9a42ac283c3a8037a
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-01 18:00:14 +0900

    Title: 십의 자리 숫자, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240701/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int[] cntArr = new int[10];
+
+        for(int i = 0; i < 100; i++){
+            arr[i] = sc.nextInt();
+            cntArr[arr[i]/10]++;
+            if(arr[i]==0){
+                break;
+            }
+        }
+
+        for(int i = 1; i < 10; i++){
+            System.out.println(i + " - " + cntArr[i]);
+        }
+
+
+    }
+}
