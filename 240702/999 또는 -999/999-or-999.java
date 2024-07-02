c21cb8fb1718741955fe648f5fe73180f17c7069
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-02 17:54:06 +0900

    Title: 999 또는 -999, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240702/999 또는 -999/999-or-999.java
+++ 240702/999 또는 -999/999-or-999.java
@@ -4,17 +4,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         int [] arr = new int[100];
-        int min = 0;
-        int max = 0;
-
-
-
         for(int i = 0; i < 100; i++){
             arr[i] = sc.nextInt();
             if(arr[i] == 999 || arr[i] == -999){
                 break;
             }
         }
+
+        int min = arr[0];
+        int max = arr[0];
 
         for(int i = 0; i<arr.length; i++){
             if(arr[i] >= max && arr[i] != 999){
