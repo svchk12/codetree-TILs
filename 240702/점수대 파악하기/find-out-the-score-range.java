ba7eedb5d9627acfe453a9c274803282d88d4557
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-02 14:52:20 +0900

    Title: 점수대 파악하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240702/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int[] cntArr = new int[10];
+
+        for(int i = 0; i < 100; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i]/10 >= 0){
+                if(arr[i] > 0){
+                    cntArr[arr[i]/10-1] ++;
+                }else{
+                    break;
+                }
+            }
+        }
+
+        for(int i = 10; i >= 1; i --){
+            System.out.println(i+"0" + " - " + cntArr[i-1]);
+        }
+
+    }
+}
