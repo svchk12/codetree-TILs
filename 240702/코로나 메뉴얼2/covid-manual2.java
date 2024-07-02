5cb6e15182f325b22e16a17f86a3bdf3e02d423e
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-02 15:03:35 +0900

    Title: 코로나 메뉴얼2, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240702/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] isCold = new String[3];
+        int[] temp = new int[3];
+        int[] cntArr = new int[4];
+        boolean situation = false;
+
+        for(int i = 0; i < 3; i++){
+            isCold[i] = sc.next();
+            temp[i] = sc.nextInt();
+            if(isCold[i].equals("Y") && temp[i] >=37){
+             cntArr[0]++;   
+            }
+            if(isCold[i].equals("N") && temp[i] >=37){
+             cntArr[1]++;   
+            }
+            if(isCold[i].equals("Y") && temp[i] < 37){
+             cntArr[2]++;   
+            }
+            if(isCold[i].equals("N") && temp[i] < 37){
+             cntArr[3]++;   
+            }
+        }
+
+        for(int i = 0; i < 4; i++){
+            System.out.print(cntArr[i] + " ");
+            if(cntArr[i]>=2){
+                situation = true;
+            }
+        }
+
+        if(situation){
+            System.out.print("E");
+        }
+
+        
+        
+    }
+}
