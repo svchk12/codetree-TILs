275478a36fd3731e187d386ba818fe76282736f4
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-04 15:17:53 +0900

    Title: 특정 원소들의 합, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240704/특정 원소들의 합/sum-of-specific-elements.java
+++ 240704/특정 원소들의 합/sum-of-specific-elements.java
@@ -12,10 +12,9 @@ public class Main {
         }
 
         int sum = 0;
-
-        for(int i = 0; i < 4; i++){
-            for(int j = 3; j >= 0; j--){
-                sum = sum + arr[i][j];
+        for(int j = 0; j < 4; j ++){
+            for(int i = 0; i + j < 4; i++){
+                sum = sum + arr[i+j][j];
             }
         }
         System.out.println(sum);
