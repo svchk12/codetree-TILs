b1f5162587d7c62e1380810f7af1a779416b9867
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-05 15:04:53 +0900

    Title: 더 긴 문자열, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240705/더 긴 문자열/longer-string.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        
+        int sl1 = s1.length();
+        int sl2 = s2.length();
+
+        if(sl1 > sl2){
+            System.out.println(s1 + " " + sl1);
+        }else{
+            System.out.println(s2 + " " + sl2);
+        }
+    }
+}
