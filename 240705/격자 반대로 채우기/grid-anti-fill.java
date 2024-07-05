c52f259ed1547a3ed7aecf599b4246ac5fb24ce3
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-05 11:06:03 +0900

    Title: 격자 반대로 채우기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240705/격자 반대로 채우기/grid-anti-fill.java
+++ 240705/격자 반대로 채우기/grid-anti-fill.java
@@ -8,19 +8,18 @@ public class Main {
         int cnt = 1;
 
         for(int j = n-1; j >=0; j--){
-            for(int i = n-1; i >=0; i --){
-                if(j % 2 != 0){
+            if(j % 2 != 0){
+                for(int i = n-1; i >= 0; i--){
                     arr[i][j] = cnt;
                     cnt++;
                 }
-                else{
-                    arr[n-i-1][j] = cnt;
+            }else{
+                for(int i = 0; i < n; i++){
+                    arr[i][j] = cnt;
                     cnt++;
                 }
-                
             }
         }
-
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 System.out.print(arr[i][j] + " ");
