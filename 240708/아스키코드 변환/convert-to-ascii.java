8957e304106099a54f79cd2e7db85c316eb8d7ba
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-08 17:56:17 +0900

    Title: 아스키코드 변환, Time: 223ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240708/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char c = a.charAt(0);
+
+        int b = sc.nextInt();
+
+        System.out.print((int)c + " ");
+        System.out.print((char)b + " ");
+        
+        
+    }
+}
