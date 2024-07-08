66d52a68ed9ba74bb20f852d71a426f5db291fdf
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-08 16:47:37 +0900

    Title: 문자 제거하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 240708/문자 제거하기/del-char.java
+++ 240708/문자 제거하기/del-char.java
@@ -6,7 +6,7 @@ public class Main {
 
         while(str.length() > 1){
             int n = sc.nextInt();
-            if(str.length() >= n){
+            if(str.length() > n){
                 str = str.substring(0, n) + str.substring(n+1);
                 System.out.println(str);
             }else{
