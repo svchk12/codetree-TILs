b9543bae7908f1beadce05bba6591e8bcbfa37e8
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-09 17:12:48 +0900

    Title: 그 다음 알파벳, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240709/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char c = a.charAt(0);
+        int next = (int)c + 1;
+        if(next == 122){
+            next = 97;
+        }
+        System.out.print((char)next);
+    }
+}
