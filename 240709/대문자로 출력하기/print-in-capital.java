2ccde212d5cf615b0e8b2355a9f1af8f83a3cef0
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-09 17:34:34 +0900

    Title: 대문자로 출력하기, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240709/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        char[] arrResult = new char[arr.length];
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] >= 'A' && arr[i] <= 'Z'){
+                arrResult[i] = arr[i];
+            }
+            if(arr[i] >= 'a' && arr[i] <= 'z'){
+                arrResult[i] = (char)(arr[i] - 'a' + 'A');
+            }
+        }
+
+        for(int i = 0; i < arrResult.length; i ++){
+            if((int)arrResult[i] != 0){
+                System.out.print(arrResult[i]);
+            }
+        }
+        
+    }
+}
