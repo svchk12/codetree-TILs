7ebbd8ef519a7d9f57fc0cda1d484dba9e40f315
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-11 16:09:15 +0900

    Title: 코드네임, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240711/코드네임/code-name.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ScoreBoard[] scoreBoard = new ScoreBoard[5];
+        for(int i = 0; i < 5; i++){
+            String codeNm = sc.next();
+            int score = sc.nextInt();
+            scoreBoard[i] = new ScoreBoard(codeNm, score);
+        }
+
+        int minIdx = 0;
+        for(int i = 0; i < 5; i++){
+            if(scoreBoard[i].score < minIdx){
+                minIdx = i;
+            }
+        }
+        System.out.println(scoreBoard[minIdx].codeNm + " " + scoreBoard[minIdx].score);
+
+    }
+}
+
+
+
+class ScoreBoard{
+    String codeNm;
+    int score;
+
+    public ScoreBoard(String codeNm, int score){
+        this.codeNm = codeNm;
+        this.score = score;
+    }
+}
