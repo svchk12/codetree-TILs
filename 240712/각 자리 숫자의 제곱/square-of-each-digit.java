f0bae7fcca3fd40a681402a7a255eea245b252e3
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-12 11:29:09 +0900

    Title: 각 자리 숫자의 제곱, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240712/각 자리 숫자의 제곱/square-of-each-digit.java
+++ 240712/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 public class Main {
-    static int sum = 0;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -10,13 +8,19 @@ public class Main {
 
     public static int cal(int n){
         
-        if(Integer.toString(n).length() == 1){
-            sum = sum + n * n;
-            return sum;
-        }else{
-            int temp = n % 10;
-            sum = sum + temp*temp;
-            return cal(n / 10);
+        // if(Integer.toString(n).length() == 1){
+        //     sum = sum + n * n;
+        //     return sum;
+        // }else{
+        //     int temp = n % 10;
+        //     sum = sum + temp*temp;
+        //     return cal(n / 10);
+        // }
+
+        if(n < 10){
+            return n * n;
         }
+        int digit = n % 10;
+        return cal(n / 10) + digit * digit;
     }
 }
