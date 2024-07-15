ff6c137b6712c04814a66d5b7b831dcf39f7531c
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-15 17:51:25 +0900

    Title: Top K 숫자 구하기, Time: 211ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240715/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        Integer [] arr = new Integer[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[k-1]);
+        
+    }
+}
