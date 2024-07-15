4b7d9dc185f042ac1f06dbc03e1ddc03dbce4d4a
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-15 15:55:44 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    static int cnt = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(cal(n));
+    }
+
+    public static int cal(int n){
+        if(n == 1){
+            return cnt;
+        }
+
+        if(n % 2 !=0){
+            cnt++;
+            return cal(3*n+1) ;
+        }else{
+            cnt++;
+            return cal(n/2);
+        }
+    }
+}
