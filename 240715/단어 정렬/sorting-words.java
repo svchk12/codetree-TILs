349b9789a8d477954be0e3401d5a66375e9cc476
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-15 17:45:43 +0900

    Title: 단어 정렬, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/단어 정렬/sorting-words.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.next();
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
