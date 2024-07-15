2f97e7707040e6e68c803e0efa00886be3d2a7d0
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-15 17:41:40 +0900

    Title: 오름 내림차순 정렬, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer [] arr = new Integer[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
