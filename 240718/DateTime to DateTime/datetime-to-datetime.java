0c3cdb8b26f44d9eb19f138134b8b05bdabd58c9
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-18 15:41:00 +0900

    Title: DateTime to DateTime, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 240718/DateTime to DateTime/datetime-to-datetime.java
+++ 240718/DateTime to DateTime/datetime-to-datetime.java
@@ -8,32 +8,10 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        
-        int date1 = 0;
-        int date2 = 0;
+        int diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11);
 
-        for(int i = 1; i <= 11; i++){
-            if(i == 11){
-                date1 = date1 + 11;  
-            }else{
-                date1 = date1 + num_of_days[i];
-            }
-        }
-
-        for(int i = 1; i <= 11; i++){
-            if(i == 11){
-                date2 = date2 + a;
-            }else{
-                date2 = date2 + num_of_days[i];
-            }
-        }
-
-        int totalMin1 = (date1 - 1) * 1440 + (60 * 11) + 11;
-        int totalMin2 = (date2 - 1) * 1440 + (60 * b) + c;
-
-        if(totalMin2 - totalMin1 >= 0){
-            System.out.print(totalMin2 - totalMin1);
+        if(diff >= 0){
+            System.out.print(diff);
         }else{
             System.out.print("-1");
         }
