dcd69d960bdc7383c5a0cd0aebe3eb07903d4928
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-18 15:58:16 +0900

    Title: 요일 맞추기, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240718/요일 맞추기/guess-day-of-week.java
+++ 240718/요일 맞추기/guess-day-of-week.java
@@ -8,39 +8,34 @@ public class Main {
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-
-        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] name_of_days = new String[]{"Sun", "Mon", "Tue", "Wed", "Fri", "Sat"};
         
-        int date1 = 0;
-        int date2 = 0;
+        String[] name_of_days = new String[]{"Sun", "Mon", "Tue", "Wed", "Fri", "Sat"};
 
-        for(int i = 1; i <= m1; i++){
-            if(i == m1){
-                date1 = date1 + d1;  
-            }else{
-                date1 = date1 + num_of_days[i];
-            }
-        }
-
-        for(int i = 1; i <= m2; i++){
-            if(i == m2){
-                date2 = date2 + d2;
-            }else{
-                date2 = date2 + num_of_days[i];
-            }
-        }
-
-        int diff = date2 - date1 + 1;
-        if(diff < 0){
-            diff = 0 - diff;
-        }
-
-        if(diff == 0){
-            System.out.println(name_of_days[1]);
+        int date1 = totalDays(m1,d1);
+        int date2 = totalDays(m2,d2);
+        int diff = 0;
+        
+        if(date1 > date2){
+            diff = date1 - date2 + 1;
+            System.out.println(name_of_days[diff % 7]);
         }else{
+            diff = date2 - date1 + 1;
             System.out.println(name_of_days[diff % 7]);
         }
+
         
+        
+        
+    }
+
+    public static int totalDays(int m, int d){
+        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int days = 0;
+
+        for(int i = 1; i < m; i++){
+            days += num_of_days[i];
+        }
+        days += d;
+        return days;
     }
 }
