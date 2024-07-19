0e6323c0e70efe603445b60beaa6a5efdd7bec3a
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-19 13:09:59 +0900

    Title: 최대로 겹치는 구간, Time: 300ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240719/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        List<Section> sections = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            Section section = new Section(start, end);
+            sections.add(section);
+        }
+
+        int minStart = sections.stream().mapToInt(Section::start).min().getAsInt();
+        int maxEnd = sections.stream().mapToInt(Section::end).max().getAsInt();
+
+        int answer = -1;
+        for (int value = minStart; value <= maxEnd; value++) {
+            int count = 0;
+            for (Section section : sections) {
+                if (section.isContain(value)) {
+                    count++;
+                }
+            }
+            
+            if (count > answer) {
+                answer = count;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
+
+class Section {
+    int start;
+    int end;
+
+    Section(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    boolean isContain(int value) {
+        return start < value && value < end;
+    }
+
+    int start() {
+        return start;
+    }
+
+    int end() {
+        return end;
+    }
+}
