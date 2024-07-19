64fc79b6356a6a94a06e88a66574c7f4b1bc205f
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-19 11:01:09 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240719/블럭쌓는 명령2/block-stacking-commands2.java
+++ 240719/블럭쌓는 명령2/block-stacking-commands2.java
@@ -7,9 +7,9 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        int[] arr = new int[n+1];
+        int[] arr = new int[n];
 
-        for(int i = 1; i < k; i++){
+        for(int i = 0; i < k; i++){
             int start = sc.nextInt();
             int end = sc.nextInt();
             for(int j = start; j <= end; j++){
