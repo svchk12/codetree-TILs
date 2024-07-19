43af3fdb4541765bf78209b8a42fa7f6b5301e88
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-19 16:20:32 +0900

    Title: 최대로 겹치는 지점, Time: 330ms, Memory: 15MB, Status: Passed - CodeTree

--- 240719/최대로 겹치는 지점/maximum-overlapped-points.java
+++ 240719/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int [] arr = new int[100];
+        int [] arr = new int[101];
 
         for(int i = 0; i < n; i++ ){
             int a = sc.nextInt();
