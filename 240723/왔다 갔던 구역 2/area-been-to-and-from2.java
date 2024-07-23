d724ba6c24f409f545e64d86ee41981640eac094
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-23 16:33:35 +0900

    Title: 왔다 갔던 구역 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240723/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[2001];
+        int start = 1000;
+        for(int i = 0; i < n; i++){
+            int x = sc.nextInt();
+            String command = sc.next();
+
+            if(command.equals("R")){
+                for(int j = 0; j <= x; j++){
+                    arr[start]++;
+                    if(j < x){
+                        start++;
+                    }
+                    
+                }
+            }else{
+                for(int j = 0; j <= x; j++){
+                    arr[start]++;
+                    if(j < x){
+                        start--;
+                    }
+                }
+            }
+        }
+
+        int cnt = 0;
+        int startIdx = 0;
+        int endIdx = 0;
+        for(int i = 0; i < 2001; i++){
+            if(arr[i] >= 2){
+            cnt++;
+        }
+    }
+    System.out.println(cnt);
+    }
+}
