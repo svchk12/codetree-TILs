f8e80d886f2229d13dd3821274def2ec051c0b48
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-30 16:27:53 +0900

    Title: 벌금은 누구에게, Time: 247ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240730/벌금은 누구에게/who-will-pay.java
+++ 240730/벌금은 누구에게/who-will-pay.java
@@ -9,13 +9,13 @@ public class Main {
         int k = sc.nextInt();
 
         int[] students = new int[n+1];
-        
         for(int i = 0; i < m; i++){
             int stNo = sc.nextInt();
             students[stNo]++;
         }
 
-        for(int i = 0; i < m; i++){
+        for(int i = 1; i < students.length; i++){
+            System.out.println(students[i]);
             if(students[i] == k){
                 System.out.println(i);
                 return;
