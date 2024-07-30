2ea8e4f383f30bfac23f3181ca4544b74343b470
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-30 14:31:29 +0900

    Title: 연속되는 수 3, Time: 186ms, Memory: 13MB, Status: Passed - CodeTree

--- 240730/연속되는 수 3/continuous-number3.java
+++ 240730/연속되는 수 3/continuous-number3.java
@@ -14,8 +14,8 @@ public class Main {
 
         int max = 0;
         int answer = 0;
-        for(int i = 0 ; i < N ; i++) {
-            if(i >= 1 && (arr[i] * arr[i - 1] > 0)) { // 부호가 같은 경우
+        for(int i = 0; i < numbers.length; i++){
+          if(i >= 1 && (numbers[i] * numbers[i - 1] > 0)) { // 부호가 같은 경우
                 max++;
             }
             else {  // 부호가 다른 경우
@@ -23,6 +23,7 @@ public class Main {
             }
             answer = Math.max(answer, max);    // 최장 길이 갱신
         }
-        System.out.println(max);
+
+        System.out.println(answer);
     }
 }
