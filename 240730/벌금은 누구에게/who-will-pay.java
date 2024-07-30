9c53731c6d5a49846c04ee4384fd0282c4036542
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-30 16:29:50 +0900

    Title: 벌금은 누구에게, Time: 221ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240730/벌금은 누구에게/who-will-pay.java
+++ 240730/벌금은 누구에게/who-will-pay.java
@@ -18,7 +18,7 @@ public class Main {
             System.out.println(students[i]);
             if(students[i] >= k){
                 System.out.println(i);
-                return;
+                break;
             }
         }
         System.out.println("-1");
