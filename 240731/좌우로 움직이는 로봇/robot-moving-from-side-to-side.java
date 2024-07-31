b8b1b458875b42d4593604cf8503cbb195df3203
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-31 16:08:58 +0900

    Title: 좌우로 움직이는 로봇, Time: 158ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240731/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240731/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -9,7 +9,7 @@ public class Main {
         int [] arr1 = new int[2000001];
         int [] arr2 = new int[2000001];
 
-        int idx = 1;
+        int idx = 0;
         int point = 1000000;
         for(int i = 0; i < n; i++){
             int t = sc.nextInt();
@@ -17,15 +17,15 @@ public class Main {
             for(int j = 0; j < t + 1; j++){
                 if(d.equals("R")){
                     point++;
-                    arr1[++idx] = point;
+                    arr1[idx++] = point;
                 }else if(d.equals("L")){
                     point--;
-                    arr1[++idx] = point;
+                    arr1[idx++] = point;
                 }
             }
         }
 
-        idx = 1;
+        idx = 0;
         point = 1000000;
         for(int i = 0; i < n; i++){
             int t = sc.nextInt();
@@ -33,14 +33,14 @@ public class Main {
             for(int j = 0; j < t + 1; j++){
                 if(d.equals("R")){
                     point++;
-                    arr2[++idx] = point;
+                    arr2[idx++] = point;
                 }else if(d.equals("L")){
                     point--;
-                    arr2[++idx] = point;
+                    arr2[idx++] = point;
                 }
             }
         }
-        int cnt = 1;
+        int cnt = 0;
         for(int i = 1; i <= idx; i++){
             if(arr1[i-1] != arr2[i-1] && arr1[i] == arr2[i]){
                 cnt++;
