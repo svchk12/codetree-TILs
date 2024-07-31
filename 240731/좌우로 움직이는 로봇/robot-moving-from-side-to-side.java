5d9eff008b9fb0d1d0d51d49446332c77c54f0eb
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-07-31 16:54:43 +0900

    Title: 좌우로 움직이는 로봇, Time: 1248ms, Memory: 49MB, Status: Passed - CodeTree

--- 240731/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240731/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -9,50 +9,55 @@ public class Main {
         int [] arr1 = new int[2000001];
         int [] arr2 = new int[2000001];
 
-        int idx = 0;
+        int idxA = 1;
         int point = 1000000;
         for(int i = 0; i < n; i++){
             int t = sc.nextInt();
             String d = sc.next();
-            for(int j = 0; j < t; j++){
+            for(int j = 1; j <= t; j++){
                 if(d.equals("R")){
                     point++;
-                    arr1[idx++] = point;
+                    arr1[idxA++] = point;
                 }else if(d.equals("L")){
                     point--;
-                    arr1[idx++] = point;
+                    arr1[idxA++] = point;
                 }
             }
         }
 
-        for(int i = idx; i < 2000001; i ++){
+        for(int i = idxA; i < 2000001; i ++){
             arr1[i] = point;
+            
         }
 
-        idx = 0;
+        int idxB = 1;
         point = 1000000;
         for(int i = 0; i < m; i++){
             int t = sc.nextInt();
             String d = sc.next();
-            for(int j = 0; j < t; j++){
+            for(int j = 1; j <= t; j++){
                 if(d.equals("R")){
                     point++;
-                    arr2[idx++] = point;
+                    arr2[idxB++] = point;
                 }else if(d.equals("L")){
                     point--;
-                    arr2[idx++] = point;
+                    arr2[idxB++] = point;
                 }
             }
         }
 
-        for(int i = idx; i < 2000001; i ++){
+        for(int i = idxB; i < 2000001; i ++){
             arr2[i] = point;
         }
 
+        int lnegth = idxA >= idxB ? idxA : idxB;
+        
         int cnt = 0;
-        for(int i = 1; i <= idx; i++){
-            if(arr1[i-1] != arr2[i-1] && arr1[i] == arr2[i]){
-                cnt++;
+        for(int i = 1; i <= lnegth; i++){
+            if(arr1[i-1] != arr2[i-1]){
+                if(arr1[i] == arr2[i]){
+                    cnt++;
+                }
             }
         }
         System.out.println(cnt);
