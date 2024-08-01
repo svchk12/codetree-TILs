b0a4eba2905c8582daa228b04f9b8da1bb64e222
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-08-01 09:22:08 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240801/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int P = sc.nextInt();
+        int T = sc.nextInt();
+
+        int[] isP = new int [N+1];
+        Situation[] situation = new Situation[251];
+
+        for(int i = 0; i < 251; i++){
+            situation[i] = new Situation();
+        }
+
+        for(int i = 0; i < T; i++){
+            int t = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            situation[t] = new Situation(x, y);
+        }
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(i + "초 : " + situation[i].x + " / " + situation[i].y);
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < 251; i++){
+            if(situation[i].x > 0){
+                if(situation[i].x == P || situation[i].y ==P){
+                    isP[situation[i].x] = 1;
+                    isP[situation[i].y] = 1;
+                }
+                if(isP[situation[i].x] == 1 || isP[situation[i].y]== 1){
+                    isP[situation[i].x] = 1;
+                    isP[situation[i].y] = 1;
+                }
+                cnt++;
+            }
+            if(cnt == K){
+                break;
+            }
+        }
+
+        for(int i = 1; i < isP.length ; i++){
+            System.out.print(isP[i]);
+        }
+    }
+}
+
+class Situation{
+    int x = 0;
+    int y = 0;
+
+    public Situation(){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Situation(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
