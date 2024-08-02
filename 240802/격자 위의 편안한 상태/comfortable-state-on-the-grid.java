7a745201f09f992bd2cb434c948d2857965242c7
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-08-02 15:00:28 +0900

    Title: 격자 위의 편안한 상태, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240802/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
+++ 240802/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int[][] arr = new int[100][100];
+        int[][] arr = new int[101][101];
 
         for(int i = 0; i < m; i++){
             int r = sc.nextInt();
