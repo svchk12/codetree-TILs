bf4ceed57d31c948c0050ab9ae6db9e9aa89e08f
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-08-02 13:02:42 +0900

    Title: 되돌아오기, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240802/되돌아오기/come-back.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+        
+        
+        int x = 0;
+        int y = 0;
+        int dir = 0;       
+        
+        int time = 0;
+        for(int i = 0; i < n; i++){
+            String d = sc.next();
+            int v = sc.nextInt();
+
+            for(int j = 0; j < v; j++){
+                dir = getDirNum(d);
+                x = x + dx[dir];
+                y = y + dy[dir];
+                time++;
+                if(x == 0 && y == 0){
+                    System.out.println(time);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static int getDirNum(String d){
+        if(d.equals("N")){
+            return 0;
+        }
+        else if(d.equals("E")){
+            return 1;
+        }
+        else if(d.equals("S")){
+            return 2;
+        }else{
+            return 3;
+        }
+    }
+
+    public static boolean inRange(int x, int y) {
+        return (0 <= x && x <= 1000 && 0 <= y && y <= 1000);
+    }
+
+
+    
+}
