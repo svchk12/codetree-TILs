0d0d07a790fc4fee1a354a2fdd6150fb39ecb1ff
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-08-03 17:41:44 +0900

    Title: 조건에 따른 연산, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240803/조건에 따른 연산/operatino-by-rule.java
+++ 240803/조건에 따른 연산/operatino-by-rule.java
@@ -13,11 +13,10 @@ public class Main {
             if(n % 2 == 0){
                 n = n * 3 + 1;
                 cnt++;
-            }    
-            if(n % 2 != 0){
+            }else{
                 n = n * 2 + 2;
                 cnt++;
-            }
+            }    
         }    
         System.out.print(cnt);
 
