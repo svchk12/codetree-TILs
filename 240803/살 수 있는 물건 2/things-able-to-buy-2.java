18c6da50983110f0e7a82bae044df310eb30306e
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-08-03 15:38:46 +0900

    Title: 살 수 있는 물건 2, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240803/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        if(a >=3000){
+            System.out.println("book");
+        }else if(a >= 1000){
+            System.out.println("mask");
+        }else if(a >= 500){
+            System.out.println("pen");
+        }else {
+            System.out.println("no");
+        }
+    }
+}
