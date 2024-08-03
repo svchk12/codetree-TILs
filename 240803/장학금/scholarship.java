66bb50600ec6d7db3923527fdfc9ffad286c8b0e
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-08-03 15:51:55 +0900

    Title: 장학금, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240803/장학금/scholarship.java
+++ 240803/장학금/scholarship.java
@@ -9,6 +9,8 @@ public class Main {
                 System.out.println("100000");
             }else if( b < 95 && b >=90){
                 System.out.println("50000");
+            }else{
+                System.out.println("0");
             }
         }else{
                 System.out.println("0");
