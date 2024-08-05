36a123b309df647dd575ce816271cfe91dfacae7
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-08-05 13:29:41 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 208ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240805/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int maxProfit = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int profit = arr[j] - arr[i];
+                if(profit > maxProfit){
+                    maxProfit = profit;
+                }
+            }
+        }
+        System.out.print(maxProfit);
+    }
+}
