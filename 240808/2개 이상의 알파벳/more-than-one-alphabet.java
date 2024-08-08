ab0f04a7b7754a5aaf15ca9d3b9579d6fda05559
svchk12 <30681894+svchk12@users.noreply.github.com>
2024-08-08 15:15:50 +0900

    Title: 2개 이상의 알파벳, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240808/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        car(str);
+    }
+
+    public static void car(String str){
+        char[] arr = str.toCharArray();
+        int cnt = 1;
+        for(int i = 1; i <= arr.length-1; i++){
+            if(arr[i-1] != arr[i]){
+                cnt++;
+            }
+        }
+
+        if(cnt >= 2){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
